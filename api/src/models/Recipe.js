00d7ec9e1220834c3732d3075a0d2444c58cc092
AnaGonzalvez <anaa.gonzalvez2@gmail.com>
2022-09-03 14:02:32 -0300

    models and db updated

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -3,10 +3,24 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('recipe', {
+  sequelize.define("recipe", {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primarykey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    summary: {
+      type: DataTypes.STRING,
+    },
+    health_score: {
+      type: DataTypes.INTEGER,
+    },
+    steps: {
+     type: DataTypes.ARRAY,
     },
   });
 };
