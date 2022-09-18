7549e047c30a19be14308c0a0393259bbb1add25
AnaGonzalvez <anaa.gonzalvez2@gmail.com>
2022-09-18 19:12:08 -0300

    components updated

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getRecipeDetail } from "../redux/actions";
+import { getRecipeDetail } from "../../redux/actions";
 import { Link } from 'react-router-dom';
 
 export default function Detail(props) {
