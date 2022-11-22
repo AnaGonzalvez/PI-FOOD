6e7c8de1ae3a9e66218e8de801628035dd23123f
AnaGonzalvez <anaa.gonzalvez2@gmail.com>
2022-11-22 12:29:12 -0300

    update deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
+axios.defaults.baseURL = process.env.REACT_APP_API;
 
 ReactDOM.render(
  <Provider store={store}>
