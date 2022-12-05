498fdf1390e224aa31955870cf0307ef295b907b
AnaGonzalvez <anaa.gonzalvez2@gmail.com>
2022-12-05 20:46:38 -0300

    styles updated

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -62,65 +62,93 @@ export default function Home(){
 
  return (
    <div className={style.container}>
-     <Nav /> 
+     <Nav />
 
      <div className={style.container2}>
-       <button onClick={(e) => handleClick(e)} className={style.btnRefresh}>Refresh Recipes</button>
+       <button onClick={(e) => handleClick(e)} className={style.btnRefresh}>
+         Refresh Recipes
+       </button>
 
-       <div className={style.sort}>
-         <label className={style.label2}>Sort by:</label>
-         <div>
-           <label className={style.label}>Alphabetic</label>
-           <select
-             name="alphabetic"
-             onClick={(e) => handleSortByAlphabet(e)}
-             className={style.select}
-           >
-             <option value='All'>Choose</option>
-             <option value="Asc">A-Z</option>
-             <option value="Desc">Z-A</option>
-           </select>
-           <p className={style.sortAlfApply}>{sortAlfApply === ''? null : sortAlfApply === 'All'? 'sort not applied': sortAlfApply === 'Asc'? 'from A to Z': 'from Z to A'}</p>
+       <div className={style.container2}>
+         <div className={style.sort}>
+           <label className={style.label2}>Sort by:</label>
+           <div>
+             <label className={style.label}>Alphabetic</label>
+             <select
+               name="alphabetic"
+               onClick={(e) => handleSortByAlphabet(e)}
+               className={style.select}
+             >
+               <option value="All">Choose</option>
+               <option value="Asc">A-Z</option>
+               <option value="Desc">Z-A</option>
+             </select>
+             <p className={style.sortAlfApply}>
+               {sortAlfApply === ""
+                 ? null
+                 : sortAlfApply === "All"
+                 ? "sort not applied"
+                 : sortAlfApply === "Asc"
+                 ? "from A to Z"
+                 : "from Z to A"}
+             </p>
+           </div>
+           <div>
+             <label className={style.label}>Health Score</label>
+             <select
+               name="health_score"
+               onClick={(e) => handleSortByHealthscore(e)}
+               className={style.select}
+             >
+               <option value="All">Choose</option>
+               <option value="LowToHigh">Low to High</option>
+               <option value="HighToLow">High to Low</option>
+             </select>
+             <p className={style.sortNumApply}>
+               {sortNumApply === ""
+                 ? null
+                 : sortNumApply === "All"
+                 ? "sort not applied"
+                 : sortNumApply === "LowToHigh"
+                 ? "from Low to High"
+                 : "from High to Low"}
+             </p>
+           </div>
          </div>
-         <div>
-           <label className={style.label}>Health Score</label>
-           <select
-             name="health_score"
-             onClick={(e) => handleSortByHealthscore(e)}
-             className={style.select}             
-           >
-             <option value='All'>Choose</option>
-             <option value="LowToHigh">Low to High</option>
-             <option value="HighToLow">High to Low</option>
-           </select>
-           <p className={style.sortNumApply}>{sortNumApply === ''? null : sortNumApply === 'All'? 'sort not applied' : sortNumApply === 'LowToHigh'? 'from Low to High' : 'from High to Low'}</p>
-         </div>
-       </div>
 
-       <div className={style.filter}>
-         <label className={style.label2}>Filter by:</label>
-         <div>
-           <label className={style.label}>Diet type</label>
-           <select
-             name="diet"
-             onClick={(e) => handleFilterByDiet(e)}
-             className={style.select}            
-           >
-             <option value='All'>Choose</option>
-             <option value="gluten free">gluten free</option>
-             <option value="ketogenic">ketogenic</option>
-             <option value="vegetarian">vegetarian</option>
-             <option value="lacto vegetarian">lacto vegetarian</option>
-             <option value="ovo vegetarian">ovo vegetarian</option>
-             <option value="lacto ovo vegetarian">lacto ovo vegetarian</option>
-             <option value="vegan">vegan</option>
-             <option value="pescetarian">pescetarian</option>
-             <option value="paleolithic">paleolithic</option>
-             <option value="primal">primal</option>
-             <option value="low FODMAP">low FODMAP</option>
-             <option value="whole 30">whole 30</option>
-           </select>
-           <p className={style.filterApply}>{filterApply === '' ? null : filterApply === 'All'? 'filter not applied' : filterApply}</p>
+         <div className={style.filter}>
+           <label className={style.label2}>Filter by:</label>
+           <div>
+             <label className={style.label}>Diet type</label>
+             <select
+               name="diet"
+               onClick={(e) => handleFilterByDiet(e)}
+               className={style.select}
+             >
+               <option value="All">Choose</option>
+               <option value="gluten free">gluten free</option>
+               <option value="ketogenic">ketogenic</option>
+               <option value="vegetarian">vegetarian</option>
+               <option value="lacto vegetarian">lacto vegetarian</option>
+               <option value="ovo vegetarian">ovo vegetarian</option>
+               <option value="lacto ovo vegetarian">
+                 lacto ovo vegetarian
+               </option>
+               <option value="vegan">vegan</option>
+               <option value="pescetarian">pescetarian</option>
+               <option value="paleolithic">paleolithic</option>
+               <option value="primal">primal</option>
+               <option value="low FODMAP">low FODMAP</option>
+               <option value="whole 30">whole 30</option>
+             </select>
+             <p className={style.filterApply}>
+               {filterApply === ""
+                 ? null
+                 : filterApply === "All"
+                 ? "filter not applied"
+                 : filterApply}
+             </p>
+           </div>
          </div>
        </div>
      </div>
@@ -134,7 +162,7 @@ export default function Home(){
      />
 
      <div className={style.cards}>
-       {currentRecipes && 
+       {currentRecipes &&
          currentRecipes.map((e) => (
            <Link to={`/home/${e.id}`} className={style.cardLink} key={e.id}>
              <Card
--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -5,33 +5,37 @@ import style from './LandingPage.module.css';
 export default function LandingPage() {
   return (
     <div className={style.container}>
-      <img
-        src="https://img.freepik.com/foto-gratis/mujer-cocina-cocina_1303-12914.jpg"
-        alt="img not found"
-        className={style.img}
-      />
-      <img
-        src="https://unareceta.com/wp-content/uploads/2016/08/receta-wok-de-verduras.jpg"
-        alt="img not found"
-        className={style.img2}
-      />
-      <img
-        src="https://img.freepik.com/foto-gratis/trozos-filete-cordero-salsas-pimiento-asado-ensalada-fresca-sobre-tabla-madera_140725-10580.jpg?w=2000"
-        alt="img not found"
-        className={style.img3}
-      />
-      <img
-        src="https://images.ecestaticos.com/kmTMxvhDeA2nNQhFwXuZ-KtLdgA=/188x5:772x443/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F4ce%2F57c%2F343%2F4ce57c343d34bf803771c15c51ed03e7.jpg"
-        alt="img not found"
-        className={style.img4}
-      />
-      <h1 className={style.title}>¡Welcome to RecipesApp!</h1>
-      <h3 className={style.subtitle}>
-        Here you will find lot of recipes to put some flavours to your food...
-      </h3>
-      <Link to="/home">
-        <button className={style.btn}>Start</button>
-      </Link>
+      <div className={style.text}>
+        <h1 className={style.title}>¡Welcome to RecipesApp!</h1>
+        <h3 className={style.subtitle}>
+          Here you will find lot of recipes to put some flavours to your food...
+        </h3>
+        <Link to="/home">
+          <button className={style.btn}>Start</button>
+        </Link>
+      </div>
+      <div className={style.images}>
+        <img
+          src="https://img.freepik.com/foto-gratis/mujer-cocina-cocina_1303-12914.jpg"
+          alt="img not found"
+          className={style.img}
+        />
+        <img
+          src="https://unareceta.com/wp-content/uploads/2016/08/receta-wok-de-verduras.jpg"
+          alt="img not found"
+          className={style.img}
+        />
+        <img
+          src="https://img.freepik.com/foto-gratis/trozos-filete-cordero-salsas-pimiento-asado-ensalada-fresca-sobre-tabla-madera_140725-10580.jpg?w=2000"
+          alt="img not found"
+          className={style.img}
+        />
+        <img
+          src="https://images.ecestaticos.com/kmTMxvhDeA2nNQhFwXuZ-KtLdgA=/188x5:772x443/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F4ce%2F57c%2F343%2F4ce57c343d34bf803771c15c51ed03e7.jpg"
+          alt="img not found"
+          className={style.img}
+        />
+      </div>
     </div>
   );
 }
